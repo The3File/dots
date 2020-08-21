5f429025f1fb7615050ac4e3d3bed622cb8d33fb
the3file <filipringdal@gmail.com>
2020-08-21 14:51:25 +0200

    installer updated
    Former-commit-id: 78f1329d64026a44aa09ba169fc23f981af142a0
    Former-commit-id: 4472fe99c3b47f828a8f63286af7b9ce703f6fd5

--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env sh
 
-config="$HOME/.dotfiles"
-config(){ /usr/bin/git --git-dir="$config" --work-tree="$HOME" $@; }
+config(){ /usr/bin/git --git-dir="$config" --work-tree="$HOME" "$@"; }
 
-git clone --bare git@github.com:The3File/dots.git "$config"
+checkout(){
+   config checkout || {
+      echo "Backing up existing files to \"$backup/\"."
+      [[ ! -d "$backup" ]] && mkdir -p "$backup"
 
-config checkout || {
-   echo "Backing up existing files to \".dotfiles-backup/\"."
-   mkdir -p .dotfiles-backup
-   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "$HOME/.dotfiles-backup/"{}
+      config checkout 2>&1 | while read line
+      do
+	 file=$(printf '%s\n' "$line" | awk {'print $1'})
+	 mv "$file" "$backup$file"
+      done
+      config checkout
+      config config status.showUntrackedFiles no
+   }
 }
-config config status.showUntrackedFiles no
+
+main(){
+   config="$HOME/.dotfiles/"
+   backup="$HOME/.dotfiles-backup/"
+
+   if [[ ! -e "$config/*" ]];then
+      git clone --bare git@github.com:The3File/dots.git "$config"
+   else
+      printf '%s\n' "exiting: $config is not empty"
+      exit 1
+   fi
+
+   checkout
+}
+
+main
