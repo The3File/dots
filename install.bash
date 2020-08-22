ae03a5bd813e9527f898894b26bf430d5c57c26b
the3file <filipringdal@gmail.com>
2020-08-22 21:05:36 +0200

    update install
    Former-commit-id: 0600eb5e7fbc3ca02bd56868cb391debb7f912a2
    Former-commit-id: 05597a8c5c1eada0afeb096235107eba6b176df7

--- install.bash
+++ install.bash
@@ -1,34 +1,18 @@
-config(){ git --git-dir="$config" --work-tree="$HOME" "$@"; }
+config="$HOME/.dotfiles"
+backup="$HOME/.dotfiles-backup"
 
-backup(){
-   printf '%s \"%s\"\n' "Conflicting files, backing to" "$backup/"
+[[ "$(ls -A $config)" ]] && { printf '%s\n' "exiting: $config is not empty"; exit 1; }
+
+config(){ git --git-dir="$config" --work-tree="$HOME" "$@"; }
+backup(){ printf '%s \"%s\"\n' "Conflicting files, backing to" "$backup/"
    [[ ! -d "$backup" ]] && mkdir -p "$backup"
 
    config checkout 2>&1 | while read -r line; do
-         file=$(printf '%s\n' "$line" | awk '{print $1}')
-      [[ -e $file ]] && {
-         mkdir -p "$backup/$file"
-         mv "$file" "$backup/$file"
-      }
-   done
-   config checkout
-   config config status.showUntrackedFiles no
-}
+      file=$(printf '%s\n' "$line" | awk '{print $1}')
+      [[ -e $file ]] && { mkdir -p "$backup/$file"
+         mv "$file" "$backup/$file"; }
+   done; config checkout; config config status.showUntrackedFiles no; }
 
-main(){
-   config="$HOME/.dotfiles"
-   backup="$HOME/.dotfiles-backup"
-   
-   printf '%s\n%s' ".dotfiles" "install.bash" >\
-      "$HOME.gitignore"
-   if [[ -d "$config" || ! -e "$config/*" ]];then
-      git clone --bare git@github.com:The3File/dots.git "$config"
-      config checkout &>/dev/null || backup
-   else
-      printf '%s\n' "exiting: $config is not empty"
-      exit 1
-   fi
-
-}
-
-main
+printf '%s\n%s\n%s' ".dotfiles" "install.bash" "README.md" > "$HOME.gitignore"
+git clone --bare git@github.com:The3File/dots.git "$config"
+config checkout &>/dev/null || backup
