527b88c5d9cceda3bd215af22fae6140d943ce58
the3file <filipringdal@gmail.com>
2020-08-22 13:29:15 +0200

    update install
    Former-commit-id: bd1ade073849c2a8729552043b56cbd7a7d3a2c6
    Former-commit-id: e97879c57bfa23bb45f44250196d17ca5d161e2a

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
 config(){ /usr/bin/git --git-dir="$config" --work-tree="$HOME" "$@"; }
 
 backup(){
-   printf '%s \"%s\"\n' "Backing up existing files to" "$backup/"
+   printf '%s \"%s\"\n' "Conflicting files, backing to" "$backup/"
    [[ ! -d "$backup" ]] && mkdir -p "$backup"
 
    config checkout 2>&1 | while read -r line; do
@@ -22,7 +22,7 @@ main(){
    printf '%s\n%s' ".dotfiles" "install.bash"
    if [[ -d "$config" || ! -e "$config/*" ]];then
       git clone --bare git@github.com:The3File/dots.git "$config"
-      config checkout || backup
+      config checkout &>/dev/null || backup
    else
       printf '%s\n' "exiting: $config is not empty"
       exit 1
