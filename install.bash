5361ae5acd12f8fe144c959c90610a4f681e10e1
the3file <filipringdal@gmail.com>
2020-08-22 13:22:18 +0200

    update install
    Former-commit-id: 7e7ab4c356bf72eefa0a892aed758cc8236f484f
    Former-commit-id: 05e2aea193cc4d919198161c33a0bfbcef69425e

--- install.bash
+++ install.bash
@@ -3,10 +3,13 @@ config(){ /usr/bin/git --git-dir="$config" --work-tree="$HOME" "$@"; }
 backup(){
    printf '%s \"%s\"\n' "Backing up existing files to" "$backup/"
    [[ ! -d "$backup" ]] && mkdir -p "$backup"
-   
+
    config checkout 2>&1 | while read -r line; do
-      file=$(printf '%s\n' "$line" | awk '{print $1}')
-      mv "$file" "$backup/$file"
+         file=$(printf '%s\n' "$line" | awk '{print $1}')
+      [[ -e $file ]] && {
+         mkdir -p "$backup/$file"
+         mv "$file" "$backup/$file"
+      }
    done
    config checkout
    config config status.showUntrackedFiles no
@@ -15,15 +18,16 @@ backup(){
 main(){
    config="$HOME/.dotfiles"
    backup="$HOME/.dotfiles-backup"
-
-   if [[ ! -e "$config/*" ]];then
+   
+   printf '%s\n%s' ".dotfiles" "install.bash"
+   if [[ -d "$config" || ! -e "$config/*" ]];then
       git clone --bare git@github.com:The3File/dots.git "$config"
+      config checkout || backup
    else
       printf '%s\n' "exiting: $config is not empty"
       exit 1
    fi
 
-   config checkout || backup
 }
 
 main
