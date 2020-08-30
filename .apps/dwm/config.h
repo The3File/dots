8e8f332320fc7361de6c46f825b9142f4e725d7d
the3file <filipringdal@gmail.com>
2020-08-30 19:33:21 +0200

    modified:   .Scripts/nm-dmenu
    Former-commit-id: 7cef51538aed47bea64f7518c8c73f1943913675
    Former-commit-id: 7fdba76d1eb5895df4952b0e281fbe090d503091

--- .apps/dwm/config.h
+++ .apps/dwm/config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Inconsolata:size=10" };
 static const char dmenufont[]       = "Inconsolata:size=10";
 static const char col_gray1[]       = "#222222";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { 
-	"terms", 
+	"1", 
 	"2", 
 	"3", 
 	"4", 
@@ -28,7 +28,7 @@ static const char *tags[] = {
 	"6", 
 	"7", 
 	"8", 
-	"web" 
+	"9" 
 };
 
 static const Rule rules[] = {
@@ -50,10 +50,10 @@ static const unsigned int gappx     = 0;        /* gap pixel between windows */
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "(@)",      spiral }, 
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "(@)",      spiral }, 
 };
 
 /* key definitions */
@@ -71,13 +71,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lfcmd[]  = { "st", "-e", "lf", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = rangercmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = lfcmd } },
 	{ MODKEY,                       XK_w,      spawn,          SHCMD ("qutebrowser") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
    	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
