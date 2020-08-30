72993b5493d9094fa3ce051e993c91bd3af739eb
the3file <filipringdal@gmail.com>
2020-08-30 19:47:24 +0000

    added install
    Former-commit-id: 3d3dd426b89515728c9c72af855507d4404c89d7
    Former-commit-id: 2d4e3dc03f220032e096dde4b0c12fc698c67d13

---
+++ install.bash
@@ -0,0 +1,18 @@
+config="$HOME/.dotfiles"
+backup="$HOME/.dotfiles-backup"
+
+[[ "$(ls -A $config)" ]] && { printf '%s\n' "exiting: $config is not empty"; exit 1; }
+
+config(){ git --git-dir="$config" --work-tree="$HOME" "$@"; }
+backup(){ printf '%s \"%s\"\n' "Conflicting files, backing to" "$backup/"
+   [[ ! -d "$backup" ]] && mkdir -p "$backup"
+
+   config checkout 2>&1 | while read -r line; do
+      file=$(printf '%s\n' "$line" | awk '{print $1}')
+      [[ -e $file ]] && { mkdir -p "$backup/$file"
+         mv "$file" "$backup/$file"; }
+   done; config checkout; config config status.showUntrackedFiles no; }
+
+printf '%s\n%s\n%s' ".dotfiles" "install.bash" "README.md" > "$HOME.gitignore"
+git clone --bare git@github.com:The3File/dots.git "$config"
+config checkout &>/dev/null || backup
