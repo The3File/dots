dbb3e81bad50a3d10f13076c7f20b3ecd15a5d6f
the3file <filipringdal@gmail.com>
2020-09-02 22:32:00 +0200

    yes
    Former-commit-id: d5990807764770f313da6192042e484d7b3b0373
    Former-commit-id: 652f42dac8d860916d9df3c1049787e48ecf4ae6

--- install.bash
+++ install.bash
@@ -4,7 +4,7 @@ backup="$HOME/.dotfiles-backup"
 [[ "$(ls -A $config)" ]] && { printf '%s\n' "exiting: $config is not empty"; exit 1; }
 
 config(){ git --git-dir="$config" --work-tree="$HOME" "$@"; }
-backup(){ printf '%s \"%s\"\n' "Conflicting files, backing to" "$backup/"
+backup(){ printf '%s \"%s\"\n' "Conflicting files, attempting backup to" "$backup/"
    [[ ! -d "$backup" ]] && mkdir -p "$backup"
 
    config checkout 2>&1 | while read -r line; do
@@ -14,5 +14,9 @@ backup(){ printf '%s \"%s\"\n' "Conflicting files, backing to" "$backup/"
    done; config checkout; config config --local status.showUntrackedFiles no; }
 
 printf '%s\n%s\n%s' ".dotfiles" "install.bash" "README.md" > "$HOME.gitignore"
-git clone --bare git@github.com:The3File/dots.git "$config"
+read -rp "[ssh/url]: "
+case $REPLY in
+   s|ssh) git clone --bare git@github.com:The3File/dots.git "$config" ;;
+   u|url) git clone --bare https://github.com/The3File/dots.git "$config"
+esac
 config checkout &>/dev/null || backup
