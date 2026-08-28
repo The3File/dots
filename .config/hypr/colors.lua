23f22332cd0844b3e41e97f4ede9007e07d05f97
Filip Ringdal <filipringdal@gmail.com>
2026-08-28 09:38:19 +0200

    M .config/hypr/colors.lua  M .config/hyprwhspr/theme/mic-osd.css

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,12 +1,12 @@
 -- Seeded from ~/.cache/wal/colors.sh (color0 inactive, color4 active)
 
 return {
-    background      = "rgb(0e1210)",
-    foreground      = "rgb(b0b9b8)",
-    color0          = "rgb(0e1210)",
-    color4          = "rgb(A84C2D)",
-    color7          = "rgb(b0b9b8)",
-    color8          = "rgb(7b8180)",
-    active_border   = "rgb(A84C2D)",
-    inactive_border = "rgb(0e1210)",
+    background      = "rgb(121210)",
+    foreground      = "rgb(c9b8b8)",
+    color0          = "rgb(121210)",
+    color4          = "rgb(586890)",
+    color7          = "rgb(c9b8b8)",
+    color8          = "rgb(8c8080)",
+    active_border   = "rgb(586890)",
+    inactive_border = "rgb(121210)",
 }
